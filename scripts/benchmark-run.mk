1d4b74762c95d28039741b1f14c7a08a118dd865
Alexey Kravets <alexey.kravets@arm.com>
2014-11-28 13:08:12 +0000

    Make sure BOARD_LIB_DIR is created.

--- scripts/benchmark-run.mk
+++ scripts/benchmark-run.mk
@@ -7,6 +7,7 @@ run-orig: ${UPLOAD_FILES_ORIG}
 	../../scripts/run-on-board.sh $^
 
 prepare: ${DATA_FILES}
+	ssh ${BOARD} "mkdir -p ${BOARD_LIB_DIR}"
 	scp ${PENCIL_RUNTIME}/libocl_pencil.so ${BOARD}:${BOARD_LIB_DIR}
 ifdef DATA_FILES
 	ssh ${BOARD} "mkdir -p ${BOARD_DATA_DIR}"
